d85da4e2050e78fdda56c1edc001e359cd0b4b1b
Neelam618 <neelamchavan410@gmail.com>
2021-04-01 22:25:37 +0530

    card image and spacing

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -9,13 +9,15 @@ import Github from './github.svg';
 function Card(props) {
     const {variant, children = 'Built a static website for a small consulting business to improve its web presence', title = 'Brain 2 Pocket', image} = props
     return (
-        <div className="bg-gray-200 p-10 shadow-lg rounded-md w-max">
+        <div className="bg-gray-200 p-10 shadow-lg rounded-md">
             <h2 className="text-center text-2xl font-semibold my-5">{title}</h2>
             <div className="shadow-lg rounded-md p-8">
-                <img src={image || DefaultImage} className="w-100 mx-auto" alt={title} />
+                <div className="shadow-lg p-4 rounded-md overflow-hidden">
+                    <img src={image || DefaultImage} className="w-full mx-auto" alt={title} />
+                </div>
                 <div className="my-6">{children}</div>
             </div>
-            <div className="text-center">
+            <div className="text-center mt-10">
                 <PrimaryBtn icon={Website} variant ='mr-4'>Launch Website</PrimaryBtn>
                 <PrimaryBtn icon={Github}>View Code</PrimaryBtn>
             </div>
