8f6d8a21676282a6e4db0981830722f83971c150
Neelam618 <neelamchavan410@gmail.com>
2021-04-01 22:12:17 +0530

    card container arguments

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,19 +2,22 @@ import React from 'react';
 import "../../index.css";
 import {PrimaryBtn} from '../ButtonWithIcon/ButtonWithIcon.stories';
 import DefaultImage from './brain2pocket.png';
+import Website from './go.svg';
+import Github from './github.svg';
+
 
 function Card(props) {
     const {variant, children = 'Built a static website for a small consulting business to improve its web presence', title = 'Brain 2 Pocket', image} = props
     return (
-        <div className="bg-gray-200 p-10 shadow-lg rounded-md">
+        <div className="bg-gray-200 p-10 shadow-lg rounded-md w-max">
             <h2 className="text-center text-2xl font-semibold my-5">{title}</h2>
             <div className="shadow-lg rounded-md p-8">
-                <img src={DefaultImage || image} className="w-100 mx-auto" alt={title} />
+                <img src={image || DefaultImage} className="w-100 mx-auto" alt={title} />
                 <div className="my-6">{children}</div>
             </div>
             <div className="text-center">
-                <PrimaryBtn variant ='mr-4'/>
-                <PrimaryBtn />
+                <PrimaryBtn icon={Website} variant ='mr-4'>Launch Website</PrimaryBtn>
+                <PrimaryBtn icon={Github}>View Code</PrimaryBtn>
             </div>
         </div>
     )
--- src/components/CardContainer/CardContainer.stories.js
+++ src/components/CardContainer/CardContainer.stories.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import {PrimaryCard} from '../Card/Card.stories';
+import StyleStage from './Style-Stage.png';
 
 export default {
-    title: 'My Work/CardContainer',
+    title: 'My Work/CardContainer'
 }
 
 export const PrimaryCardContainer = () => (
     <div className="grid grid-flow-col grid-cols-2 gap-10 m-5">
-        <PrimaryCard title = 'Style Stage'>regfref ewfeu</PrimaryCard>
+        <PrimaryCard title = 'Style Stage' image={StyleStage}>regfref ewfeu</PrimaryCard>
         <PrimaryCard title = 'Movies browser'>regfref Movies browser</PrimaryCard>
 
     </div>
