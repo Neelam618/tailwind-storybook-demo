9f284172aa5903a27bb94c55370581b4357eb4aa
Neelam618 <neelamchavan410@gmail.com>
2021-04-01 17:23:03 +0530

    Card container grid

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,7 +6,7 @@ import DefaultImage from './brain2pocket.png';
 function Card(props) {
     const {variant, children = 'Built a static website for a small consulting business to improve its web presence', title = 'Brain 2 Pocket', image} = props
     return (
-        <div className="bg-gray-200 w-5/12 p-10 shadow-lg rounded-md m-8">
+        <div className="bg-gray-200 p-10 shadow-lg rounded-md">
             <h2 className="text-center text-2xl font-semibold my-5">{title}</h2>
             <div className="shadow-lg rounded-md p-8">
                 <img src={DefaultImage || image} className="w-100 mx-auto" alt={title} />
--- src/components/CardContainer/CardContainer.stories.js
+++ src/components/CardContainer/CardContainer.stories.js
@@ -2,11 +2,11 @@ import React from 'react';
 import {PrimaryCard} from '../Card/Card.stories';
 
 export default {
-    title: 'CardContainer',
+    title: 'My Work/CardContainer',
 }
 
 export const PrimaryCardContainer = () => (
-    <div>
+    <div className="grid grid-flow-col grid-cols-2 gap-10 m-5">
         <PrimaryCard title = 'Style Stage'>regfref ewfeu</PrimaryCard>
         <PrimaryCard title = 'Movies browser'>regfref Movies browser</PrimaryCard>
 
